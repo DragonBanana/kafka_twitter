39b67cc8575d298865ed8baac5085370b2432067
Luca Ferrera <luca.ferrera96@gmail.com>
2019-03-16 16:51:21 +0100

    Custom Tweet partition created

--- kafka_middleware/src/main/java/kafka/partitioner/TweetPartitioner.java
+++ kafka_middleware/src/main/java/kafka/partitioner/TweetPartitioner.java
@@ -2,6 +2,7 @@ package kafka.partitioner;
 
 import org.apache.kafka.clients.producer.Partitioner;
 import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.PartitionInfo;
 
 import java.util.Map;
 
@@ -9,8 +10,35 @@ public class TweetPartitioner implements Partitioner {
 
 
     @Override
-    public int partition(String s, Object o, byte[] bytes, Object o1, byte[] bytes1, Cluster cluster) {
+    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
         //TODO
+        List<PartitionInfo> partitions = cluster.partitionsForTopic(topic);
+        int numPartitions = partitions.size();
+
+        Tweet tweet = ((Tweet)value);
+        String author = tweet.getAuthor();
+        int hashAuthor = author.hashCode();
+        if(topic.equals("location")){
+            String location = tweet.getLocation();
+            int hashLocation = location.hashCode();
+            return (hashAuthor + hashLocation)%(numPartitions-1);
+        }
+        if(topic.equals("mention")){
+            if (tweet.getMentions().size() == 1){
+                String mention = tweet.getMentions()[0];
+                int hashMention = mention.hashCode();
+                return (hashAuthor + hashMention)%(numPartitions-1);
+            }
+            return numPartitions;
+        }
+        if(topic.equals("tag")){
+            if (tweet.getTags().size() == 1){
+                String tag = tweet.getTags()[0];
+                int hashTag = tag.hashCode();
+                return (hashAuthor + hashTag)%(numPartitions-1);
+            }
+            return numPartitions;
+        }
         return 1;
     }
 
