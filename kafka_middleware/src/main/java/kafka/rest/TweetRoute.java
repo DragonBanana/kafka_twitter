690796c2642d903d646bba70895b2403e3126575
SwagMaster <10493858@polimi.it>
2019-04-01 20:51:48 +0200

    Bozza di sse

--- kafka_middleware/src/main/java/kafka/rest/TweetRoute.java
+++ kafka_middleware/src/main/java/kafka/rest/TweetRoute.java
@@ -3,7 +3,12 @@ package kafka.rest;
 import com.google.gson.Gson;
 import kafka.model.Tweet;
 import kafka.model.Twitter;
+import spark.Response;
+import spark.servlet.SparkApplication;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -43,12 +48,9 @@ public class TweetRoute {
             response.type("application/json");
             response.status(200);
 
-            List<String> locations = new ArrayList<>();
-            locations.addAll(Arrays.asList(request.splat()[0].split("&")));
-            List<String> tags = new ArrayList<>();
-            tags.addAll(Arrays.asList(request.splat()[1].split("&")));
-            List<String> mentions = new ArrayList<>();
-            mentions.addAll(Arrays.asList(request.splat()[2].split("&")));
+            List<String> locations = new ArrayList<>(Arrays.asList(request.splat()[0].split("&")));
+            List<String> tags = new ArrayList<>(Arrays.asList(request.splat()[1].split("&")));
+            List<String> mentions = new ArrayList<>(Arrays.asList(request.splat()[2].split("&")));
 
             return new Gson().toJson(new TweetStub().findTweets(id, locations, tags, mentions));
             //TODO check error in filters
@@ -74,5 +76,70 @@ public class TweetRoute {
             return new Gson().toJson(new TweetStub().findTweets(id, locations, tags, mentions));
             //TODO check error in filters
         });
+
+        get("/sse",(request, response) -> {
+
+
+            System.out.println("richiesta ricevuta");
+
+            response.header("Connection", "keep-alive");
+            response.header("Content-Type", "text/event-stream");
+            response.header("Cache-control", "no-cache");
+            response.raw().setContentType("text/event-stream");
+            response.header("Access-Control-Allow-Origin", "*");
+            response.status(200);
+            //OutputStream out = null;
+            //try {
+                //out = response.raw().getOutputStream();
+            //} catch (IOException e) {
+            //    e.printStackTrace();
+            //}
+
+            response.raw().setStatus(200);
+            response.raw().setContentType("text/event-stream");
+            //response.raw().setHeader("Access-Control-Allow-Origin", "*");
+            response.raw().setHeader("Connection", "keep-alive");
+            response.raw().setHeader("Content-Type", "text/event-stream");
+            response.raw().setHeader("Cache-control", "no-cache");
+            PrintWriter p = response.raw().getWriter();
+            p.print("id: 99\nevent: eventType\ndata: CIAO\n\n\n");
+            p.flush();
+
+
+
+            new Thread(() -> {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //send(response);
+            }).start();
+
+            return "id: 99\nevent: eventType\ndata: RETURN \n\n\n";
+        });
+    }
+
+    private static void send(Response response) {
+
+        for(int i = 0; i < 1; i++) {
+            OutputStream out = null;
+            try {
+                out = response.raw().getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            response.raw().setStatus(200);
+            response.raw().setContentType("text/event-stream");
+            //response.raw().setHeader("Access-Control-Allow-Origin", "*");
+            response.raw().setHeader("Connection", "keep-alive");
+            response.raw().setHeader("Content-Type", "text/event-stream");
+            response.raw().setHeader("Cache-control", "no-cache");
+            PrintWriter p = new PrintWriter(out);
+            p.print("id: 99\nevent: eventType\ndata: CIAO\n\n\n");
+
+
+        }
     }
 }
