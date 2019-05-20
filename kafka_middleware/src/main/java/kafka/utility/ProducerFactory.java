0ae7f6c96e03c68ea3510ae02b9d35392fafc305
SwagMaster <10493858@polimi.it>
2019-05-21 00:21:22 +0200

    Server port changed.

--- kafka_middleware/src/main/java/kafka/utility/ProducerFactory.java
+++ kafka_middleware/src/main/java/kafka/utility/ProducerFactory.java
@@ -32,7 +32,7 @@ public class ProducerFactory {
      */
     private static Properties getDefaultProperty() {
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "23.97.231.221:32775");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "23.97.231.221:32777");
         //TODO Check what transactional id has to be assigned
         String transactionId = Double.toString(Math.abs(Math.random()));
         props.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, transactionId);
