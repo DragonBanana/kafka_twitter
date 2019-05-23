90f94d89079f8b05c2c53e99ab21f81b1628d777
SwagMaster <10493858@polimi.it>
2019-05-23 19:17:11 +0200

    Configuring consumer in order to be wait for poll longer.

--- kafka_middleware/src/main/java/kafka/utility/ConsumerFactory.java
+++ kafka_middleware/src/main/java/kafka/utility/ConsumerFactory.java
@@ -3,11 +3,18 @@
  import org.apache.kafka.clients.consumer.Consumer;
  import org.apache.kafka.clients.consumer.ConsumerConfig;
  import org.apache.kafka.clients.consumer.KafkaConsumer;
+ import org.apache.kafka.common.TopicPartition;
  import org.apache.kafka.common.serialization.StringDeserializer;
 
- import java.util.Properties;
+ import java.time.Duration;
+ import java.util.*;
 
-public class ConsumerFactory {
+ public class ConsumerFactory {
+
+    /**
+     * The number of consumer per group.
+     */
+    private static final int N_CONSUMER = 3;
 
     /**
      * Return the consumer.
@@ -16,6 +23,32 @@ public class ConsumerFactory {
     public static Consumer<String, String> getConsumer() {
         return new KafkaConsumer<>(getDefaultProperty());
     }
+
+    /**
+     * Returns a list of consumers in the same consumer group.
+     * @param groupId the consumer group id
+     * @return a list of consumers in the same consumer group.
+     */
+    public static List<Consumer<String, String>> getConsumerGroup(String topic, String groupId) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "23.97.231.221:32778");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100000");
+        props.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        List<Consumer<String, String>> consumerList = new ArrayList<>();
+        for(int i = 0; i < N_CONSUMER; i++) {
+            consumerList.add(new KafkaConsumer<>(getDefaultProperty()));
+        }
+        consumerList.forEach(c -> {
+            c.subscribe(Collections.singletonList(topic));
+            c.poll(Duration.ofMillis(0));
+        });
+        return consumerList;
+    }
+
 
     /**
      * Return the default producer properties.
